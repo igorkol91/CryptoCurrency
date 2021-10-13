043974d4401a65179a18d2a1ad7d46cd76b1a4e2
Igor.Koloski <koloski_igor@hotmail.com>
2021-10-13 23:09:23 +0200

    Add filter by name search bar

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Logos from './Logos';
 
 const Links = () => {
   const state = useSelector((state) => state);
+  const [filter, setFilter] = useState('');
   let i = 1;
   let colorBool = true;
   let bgColor = 'light-blue';
-  const List = (state.value).map((elem) => {
+
+  const List = ((state.value).filter((elem) => {
+    if (filter === '') {
+      return elem;
+    }
+    if (elem.name.toString().toLowerCase().includes(filter.toString().toLowerCase())) {
+      return elem;
+    }
+    return '';
+  })).map((elem) => {
     if (i === 2) {
       colorBool = !colorBool;
       if (colorBool) {
@@ -20,6 +30,7 @@ const Links = () => {
       }
     }
     i += 1;
+
     return (
       <Link key={elem.id} to={elem.id} className={`text-decoration-none col-6 home-links ${bgColor}`}>
         <Logos name={elem.name} logoUrl={elem.logo_url} rank={elem.rank} />
@@ -32,8 +43,9 @@ const Links = () => {
       <div className="col-6 home-links dark-blue d-flex justify-content-center align-items-center">
         <h3 className="home-links-text">TOP 100 Crypto Currencies</h3>
       </div>
-      <div className="col-6 home-links light-blue">
-        <img src="https://image.shutterstock.com/image-vector/world-map-260nw-735333076.jpg" alt="img" className="main-img w-100 h-100" />
+      <div className="col-6 home-links light-blue d-flex flex-column justify-content-center">
+        <h3 className="text-white">Search by name</h3>
+        <input type="text" placeholder="Search..." onChange={(event) => { setFilter(event.target.value); }} />
       </div>
       {List}
     </div>
