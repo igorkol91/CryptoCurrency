a0aa0ec4e1c6a8093956e56e569725d582ffee46
Igor.Koloski <koloski_igor@hotmail.com>
2021-10-15 11:54:21 +0200

    Add cors mode

--- src/redux/reducers/cryptoReducers.js
+++ src/redux/reducers/cryptoReducers.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 
 const loadCrypto = createAsyncThunk('fetchData',
   async () => {
-    const result = await fetch('https://api.nomics.com/v1/currencies/ticker?key=d4a4c47a532e48347da5501fd19f66e912b4bc09&gdax&per-page=100&interval=1dgdax&interval=1d&per-page=20', { method: 'GET', mode: 'CORS' });
+    const result = await fetch('https://api.nomics.com/v1/currencies/ticker?key=d4a4c47a532e48347da5501fd19f66e912b4bc09&gdax&per-page=100&interval=1dgdax&interval=1d&per-page=20', { method: 'GET', mode: 'cors' });
     const res = await result.json();
     return res;
   });
