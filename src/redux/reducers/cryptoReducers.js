81b6b3376e337ed0e45a58290855c4e331ecc37c
Igor.Koloski <koloski_igor@hotmail.com>
2021-10-16 14:35:39 +0200

    Add headers file

--- src/redux/reducers/cryptoReducers.js
+++ src/redux/reducers/cryptoReducers.js
@@ -1,15 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-};
-
 const loadCrypto = createAsyncThunk('fetchData',
   async () => {
-    const result = await fetch('https://api.nomics.com/v1/currencies/ticker?key=d4a4c47a532e48347da5501fd19f66e912b4bc09&gdax&per-page=100&interval=1dgdax&interval=1d&per-page=20', { method: 'GET', mode: 'cors', headers });
+    const result = await fetch('https://api.nomics.com/v1/currencies/ticker?key=d4a4c47a532e48347da5501fd19f66e912b4bc09&gdax&per-page=100&interval=1dgdax&interval=1d&per-page=20', { method: 'GET', mode: 'cors' });
     const res = await result.json();
     return res;
   });
